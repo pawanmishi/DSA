74bee0dc9356dcd53862acc18f23e17280ceee7e
pawanmishi <pmish009@gmail.com>
2022-11-18 11:13:47 +0530

    Add files via upload

---
+++ Max-Min/maxmin2.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+main()
+{
+
+    int n, max, min, number, i;
+    scanf("%d", &n);
+    if (n >0)
+    {
+        scanf("%d", &number);
+        max=number;
+        min=max;
+        for(i=1; i<n; i++)
+        {
+            scanf("%d", &number);
+            if (number > max) max=number;
+            else
+                {
+            if (number < min) min=number;
+                }
+        }
+        printf("Maximum  is %d \n ", max);
+        printf("Minimum  is %d", min);
+
+    }
+}
